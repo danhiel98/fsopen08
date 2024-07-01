7c7dfd523cbab568c50913e64ac5601549c6ba12
danhiel98 <danhiel.garcia98@gmail.com>
2024-06-30 20:53:11 -0600

    Exercises 8.13.-8.16.

---
+++ library/models/book.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const uniqueValidator = require('mongoose-unique-validator')
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 5,
+    unique: true
+  },
+  published: {
+    type: Number,
+  },
+  genres: [
+    { type: String }
+  ],
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author'
+  }
+})
+
+schema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('Book', schema)
